68495db59adc8aaba85507a77b061c8d30ee4a55
= <denis.biguenet@gmail.com>
2020-01-17 12:55:53 +0100

    Modify report

--- src/Controller/FileUtils.cpp
+++ src/Controller/FileUtils.cpp
@@ -9,20 +9,26 @@
  * FileUtils implementation
  */
 
+ /* Charge un niveau selon le nom du fichier spécifié
+ * fileName : nom du niveau à charger
+ * renvoie un vecteur de points qui correspond à la figure à reproduire
+ */
 std::vector<Point<double>> FileUtils::readFile(const char* fileName) {
-    std::vector<Point<double>> points;
-    std::string line;
-    std::ifstream level(fileName);
-    string xStr, yStr;
-    double x, y;
-    int sep;
+    std::vector<Point<double>> points; // Liste des points de la forme à reproduire
+    std::string line; // contient la dernière ligne lue
+    std::ifstream level(fileName); // ouvre le fichier en lecture
+    string xStr, yStr; // contiennent les coordonnées du points
+    double x, y; // contiennent les coordonnées du points sous forme de nombres
+    int sep; // indice de la séparation entre les coordonnées x et y (un espace)
     if(level) {
-        while(getline(level, line)) {
-            sep = line.find(' ');
+        while(getline(level, line)) { // On lit une ligne du fichier
+            sep = line.find(' '); // On cherche le premier espace pour séparer la coordonnée x et y
             xStr = line.substr(0, sep);
             yStr = line.substr(sep, line.size());
+            // On utilise stoi pour passer de string vers double
             x = std::stoi(xStr);
             y = std::stoi(yStr);
+            // On ajoute le point dans le vecteur
             points.push_back(Point(x, y));
         }
     }
