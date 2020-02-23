0a819d4b13727cedea3d38a735285514052b3b90
= <denis.biguenet@gmail.com>
2020-02-23 13:23:23 +0100

    Add doxygen documentation for Button class.

--- include/View/Button.h
+++ include/View/Button.h
@@ -1,5 +1,8 @@
-/**
- * Project Tangram
+/*!
+ * \file Button.h
+ * \brief Defines a representation of buttons.
+ * \author Biguenet Denis & Gosset Severin
+ * \date 22/02/2020
  */
 
 
@@ -12,22 +15,64 @@
 #include "../../include/Controller/Clickable.h"
 #include "../../include/Model/Point.h"
 
-
+/*! \class Button
+ * \brief Representation of button
+ *
+ * A button is represented by two points (top left point and bottom right point), a text, a lambda function called when the button is selected and sfml rectangle and text.
+ */
 class Button: public Drawable, public Clickable<double> {
 public :
+    /*!
+     * \brief default constructor
+     * \param topLeftX : X coordinate of top left point
+     * \param topLeftY : Y coordinate of top left point
+     * \param bottomRightX : X coordinate of bottom right point
+     * \param bottomRightY : Y coordinate of bottom right point
+     * \param text : string that contains the button's text
+     * \param fct : lambda function called when the button is clicked
+     *
+     */
     Button(int topLeftX, int topLeftY, int bottomRightX, int bottomRightY, std::string text, std::function<void()> _fct);
+
+    /*!
+     * \brief destructor to delete txt field's font
+     *
+     */
     ~Button();
 
+    /*!
+     * \brief Calls the lambda function of the button
+     *
+     */
     void callFct();
+
+    /*!
+     * \brief Draw method
+     * \param window : window where the button is draw
+     *
+     */
     void draw(sf::RenderWindow& window) override;
+
+    /*!
+     * \brief Check if the button is clicked bu the user
+     * \param p : the position of the click
+     * \return true if the button is clicked, false in the other case
+     *
+     */
     bool isClicked(const Point<double>& p) const override;
+
+    /*!
+     * \brief Gets the center of the button
+     * \return the center of the button
+     *
+     */
     Point<double> center() const;
 
 private :
-    Point<double> topLeft;
-    Point<double> bottomRight;
-    std::string text;
-	std::function<void ()> fct;
-	std::unique_ptr<sf::RectangleShape> rectangle;
-	std::unique_ptr<sf::Text> txt;
+    Point<double> topLeft; /*!Top left point*/
+    Point<double> bottomRight; /*!Right bottom point*/
+    std::string text; /*!Button's text*/
+	std::function<void ()> fct; /*!Lambda function called when the button is clicked*/
+	std::unique_ptr<sf::RectangleShape> rectangle; /*!SFML rectangle to draw the button*/
+	std::unique_ptr<sf::Text> txt; /*!SFML text to draw the button's content*/
 };
